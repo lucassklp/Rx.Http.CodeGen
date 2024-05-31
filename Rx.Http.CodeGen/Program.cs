3df57ead3f586354eb81a9fe8d3d686ca75ab614
Lucas Simas <simas.lucas@hotmail.com>
2024-05-31 12:29:43 +0100

    refactor and constructor gen

--- Rx.Http.CodeGen/Program.cs
+++ Rx.Http.CodeGen/Program.cs
@@ -35,25 +35,41 @@ var typesMap = new Dictionary<string, string>()
 
 
 //Generate model classes (schemas)
-foreach (var schema in openApiDocument.Components.Schemas)
+ClassGen GenerateModelClasses(string name, OpenApiSchema schema)
 {
-    var modelClassGen = new ClassGen(name: schema.Key.ToPascalCase())
+    var modelClassGen = new ClassGen(name: name.ToPascalCase())
         .Namespace(@namespace)
         .Public();
 
-    var properties = schema.Value.Properties;
+    var properties = schema.Properties;
 
     foreach (var property in properties)
     {
         var type = typesMap[property.Value.Type];
+
+        if(type == "object")
+        {
+            type = property.Value.Reference.Id;
+        }
+        else if(type == "List<object>")
+        {
+            var subtype = property.Value?.Items?.Reference?.Id ?? property.Value?.Items?.Type;
+            type = $"List<{subtype}>";
+        }
 
         var fieldGen = new FieldGen(name: property.Key.ToPascalCase(), type: type)
             .Public();
 
         modelClassGen.WithField(fieldGen);
     }
-
     Console.WriteLine(modelClassGen.GenerateCode());
+    return modelClassGen;
+}
+
+
+foreach (var schema in openApiDocument.Components.Schemas)
+{
+    GenerateModelClasses(schema.Key, schema.Value);
 }
 
 
@@ -61,68 +77,84 @@ foreach (var schema in openApiDocument.Components.Schemas)
 var classGen = new ClassGen("OpenApiPetStoreConsumer")
     .Extends("RxHttpClient")
     .Namespace(@namespace)
-    .Using("Rx.Http", "Rx.Http.Extensions");
+    .Using("System", "Rx.Http", "Rx.Http.Extensions");
+
+void GenerateConstructor(ClassGen classGen, OpenApiDocument openApi)
+{
+    classGen.Constructor(ctor => 
+    {
+        ctor.Public()
+            .WithParameter("HttpClient", "httpClient")
+            .WithBase("httpClient", "null");
+        
+    });
+}
+
+void GenerateMethod(string path, string httpMethod, OpenApiOperation operation)
+{
+    OpenApiSchema? schema = null;
+    try
+    {
+        schema = operation.Responses["200"]?.Content["application/json"]?.Schema;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"failed to {httpMethod} {path} - {ex.Message}");
+    }
+
+    string type = schema?.Reference?.Id ?? "object";
+
+    if(schema?.Type == "array")
+    {
+        type = $"List<{schema.Items.Reference.Id}>";
+    }
+
+    var body = string.Empty;
+
+    var methodGen = new MethodGen(name: operation.OperationId.ToPascalCase(), returnType: $"IObservable<{type}>")
+        .Public();
+        
+    if (operation.RequestBody is null)
+    {
+        body = $"""
+            return {httpMethod.ToPascalCase()}<{type}>($"{path}");
+            """;
+    }
+    else
+    {
+        var bodyType = "object";
+
+        if (operation.RequestBody.Content.Any(x => x.Key == "application/json"))
+        {
+            bodyType = typesMap[operation.RequestBody.Content["application/json"].Schema.Type];
+        }
+        
+        methodGen.WithParameter(bodyType, "body");
+        body = $"""
+            return {httpMethod.ToPascalCase()}<{type}>($"{path}", body);
+            """;
+    }
+
+    methodGen.WithBody(body);
+
+    foreach (var parameter in operation.Parameters)
+    {
+        var name = parameter.Name;
+        var paramType = typesMap[parameter.Schema.Type];
+        methodGen.WithParameter(name: name, type: paramType);
+            
+    }
+    classGen.WithMethod(methodGen);
+}
+
+GenerateConstructor(classGen, openApiDocument);
 
 foreach(var path in openApiDocument.Paths)
 {
     foreach(var operation in path.Value.Operations)
     {
-        OpenApiSchema schema = null;
-        try
-        {
-            schema = operation.Value.Responses["200"]?.Content["application/json"]?.Schema;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"failed to {path.Key} {operation.Key}. {ex.Message}");
-        }
-
-        string type = schema?.Reference?.Id ?? "void";
-
-        if(schema?.Type == "array")
-        {
-            type = $"List<{schema.Items.Reference.Id}>";
-        }
-
-        var body = string.Empty;
-
-        var methodGen = new MethodGen(name: operation.Value.OperationId.ToPascalCase(), returnType: $"Observable<{type}>")
-            .Public();
-            
-        if (operation.Value.RequestBody is null)
-        {
-            body = $"""
-                return {operation.Key.GetDisplayName().ToPascalCase()}<{type}>($"{path.Key}");
-                """;
-        }
-        else
-        {
-            var bodyType = "object";
-
-            if (operation.Value.RequestBody.Content.Any(x => x.Key == "application/json"))
-            {
-                bodyType = typesMap[operation.Value.RequestBody.Content["application/json"].Schema.Type];
-            }
-            
-            methodGen.WithParameter(bodyType, "body");
-            body = $"""
-                return {operation.Key.GetDisplayName().ToPascalCase()}<{type}>($"{path.Key}", body);
-                """;
-        }
-
-        methodGen.WithBody(body);
-
-        foreach (var parameter in operation.Value.Parameters)
-        {
-            var name = parameter.Name;
-            var paramType = typesMap[parameter.Schema.Type];
-            methodGen.WithParameter(name: name, type: paramType);
-                
-        }
-        classGen.WithMethod(methodGen);
+        GenerateMethod(path.Key, operation.Key.GetDisplayName(), operation.Value);
     }
 }
 
 Console.WriteLine(classGen.GenerateCode());
-
-Console.ReadKey();
