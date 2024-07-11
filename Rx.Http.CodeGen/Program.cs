e184a9ff3e1cd2f698f9a3c1d0297aa550fba461
Lucas Simas <simas.lucas@hotmail.com>
2024-07-11 17:01:01 +0100

    refactor: Improve code quality

--- Rx.Http.CodeGen/Program.cs
+++ Rx.Http.CodeGen/Program.cs
@@ -1,89 +1,22 @@
 ﻿using Rx.Http.CodeGen;
-using Rx.Http;
 using System.Reactive.Linq;
 using CommandLine;
-using CaseConverter;
 
-Parser.Default.ParseArguments<ConsumerGenerationOptions>(args)
+Parser.Default.ParseArguments<ConsumerGenerationArgs>(args)
     .WithParsed(options =>
     {
         Logger.IsVerbose = options.Verbose;
-
-        string? openApiDefinition = null;
-        var initialPath = Directory.GetCurrentDirectory();
-        string defaultType = "object";
-        if (!string.IsNullOrEmpty(options.Url))
+        var configuratorProcessor = new ConfigurationProcessor(options);
+        try
         {
-            Logger.Log($"Fetching {options.Url}");
-            try
-            {
-                var httpClient = RxHttpClient.Create();
-                openApiDefinition = httpClient.Get(options.Url)
-                    .SelectMany(httpResp => httpResp.Content.ReadAsStringAsync())
-                    .Wait();
-            }
-            catch (Exception ex)
-            {
-                Logger.Log($"An error occurred when fetching {options.Url}: {ex.Message}", ConsoleColor.Red);
-                Logger.LogVerbose(ex.StackTrace, ConsoleColor.Red);
-                return;
-            }
+            var config = configuratorProcessor.Process();
+            var consumerGenerator = new ConsumerGenerator(config);
+            consumerGenerator.GenerateFiles();
         }
-        else if (!string.IsNullOrEmpty(options.File))
+        catch (Exception ex)
         {
-            try
-            {
-                Logger.Log($"Reading {options.File}");
-                if (options.File?.StartsWith(".") ?? false)
-                {
-                    openApiDefinition = File.ReadAllText(Path.Combine(initialPath, options.File));
-                }
-                else
-                {
-                    openApiDefinition = File.ReadAllText(options.File!);
-                }
-            }
-            catch (Exception ex) 
-            {
-                Logger.Log($"An error occurred when reading {options.File}: {ex.Message}", ConsoleColor.Red);
-                Logger.LogVerbose(ex.StackTrace, ConsoleColor.Red);
-                return;
-            }
-        }
-
-        if(options.Type == "dictionary")
-        {
-            defaultType = "IDictionary<string, object>";
-        }
-
-        if (!string.IsNullOrEmpty(openApiDefinition))
-        {
-            Logger.Log($"Trying to generate the code");
-            Logger.LogVerbose($"OpenApi definition read: {openApiDefinition}");
-
-            try
-            {
-                var consumerConfig = new ConsumerGenerationConfig
-                {
-                    Path = Path.Combine(initialPath, options.Namespace!),
-                    Namespace = options.Namespace,
-                    OpenApiDefinition = openApiDefinition,
-                    ConsumerName = options.Output?.ToPascalCase() ?? "",
-                    Type = defaultType,
-                    Verbose = options.Verbose
-                };
-
-                var consumerGen = new ConsumerGenerator(consumerConfig);
-                consumerGen.GenerateFiles();
-
-                Logger.Log("Code generated successfully!", ConsoleColor.Green);
-                return;
-            }
-            catch (Exception ex)
-            {
-                Logger.Log($"An error occurred when generating files: {ex.Message}", ConsoleColor.Red);
-                Logger.LogVerbose(ex.StackTrace, ConsoleColor.Red);
-                return;
-            }
+            Logger.Log(ex.Message, ConsoleColor.Red);
+            Logger.LogVerbose(ex.StackTrace, ConsoleColor.Red);
         }
     });
+
